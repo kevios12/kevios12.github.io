8a23c72d50c6b67a0313a45cadfc5492f99ca01b
kevios12 <kerschbaumer.kevin2@outlook.de>
2022-05-19 01:58:37 +0200

    create 'our team/about us' section

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, "./src/index.html"),
                 about: resolve(__dirname, "./src/about.html"),
+                team: resolve(__dirname, "./src/team.html"),
             },
         },
     }
